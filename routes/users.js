08bf7ad5e80ae948b03815c13f8a51ba33bf8cc1
Sajjad Ali <48791596+ten3live@users.noreply.github.com>
2023-03-25 19:59:23 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -71,8 +71,6 @@ router.get("/me", auth, async (req, res) => {
 
 //router.post("/", upload.single("image"), async (req, res) => {
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
   if (user) {
@@ -91,11 +89,7 @@ router.post("/", async (req, res) => {
     mobile: req.body.mobile,
     address: req.body.address,
   });
-  user.save((error) => {
-    if (error) {
-      return res.status(500).json({ error });
-    }
-  });
+
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
