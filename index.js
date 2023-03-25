ce8085643d0072bc81663ec2aae814b84dc544e9
Sajjad Ali <48791596+ten3live@users.noreply.github.com>
2023-03-25 19:45:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (!config.get("jwtPrivateKey")) {
   process.exit(1);
 }
 mongoose
-  .connect("mongodb+srv://ten3live:sajjadali7@sajjadsolangi.eduls.mongodb.net/test")
+  .connect("mongodb+srv://ten3live:sajjadali7@cluster0.aozjroo.mongodb.net/myDatabase")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.log("Could not connect to MongoDB..."));
 
